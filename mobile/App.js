40c4a9f52fa9d2c10d96478af138a4620c5cb771
Robin Dijoux <dr@ecomail.fr>
2023-01-13 09:59:10 +0100

    init

---
+++ mobile/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(count + 1);
+
+  return (
+    <View style={styles.container}>
+      <TouchableHighlight onPress={onPress}>
+        <View style={styles.button}>
+          <Text>Touch ntgm Here</Text>
+        </View>
+      </TouchableHighlight>
+      <View style={styles.countContainer}>
+        <Text style={styles.countText}>{count || null}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10,
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10,
+  },
+  countText: {
+    color: "#FF00FF",
+  },
+});
