861b89aa07fd962f6f1c16aecc33aeb17255a4da
ThibaultGrt <thibault.garrot@gmail.com>
2023-02-06 10:31:44 +0100

    Création de la page d'identification front-end

--- mobile/App.js
+++ mobile/App.js
@@ -1,40 +1,87 @@
+import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
-
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TextInput,
+    Button,
+    TouchableOpacity,
+} from "react-native";
 export default function App() {
-  const [count, setCount] = useState(0);
-  const onPress = () => setCount(count + 1);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={require("./assets/polytech.jpeg")} />
+            <StatusBar style="dark" />
+            <Text>Veuillez vous identifier avec votre : {"\n"} - numéro étudiant {"\n"} - mail universitaire (professeur) {"\n"}</Text>
 
-  return (
-    <View style={styles.container}>
-      <TouchableHighlight onPress={onPress}>
-        <View style={styles.button}>
-          <Text>Touch ntgm Here</Text>
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="N° étudiant ou mail"
+                    placeholderTextColor="#003f5c"
+                    onChangeText={(email) => setEmail(email)}
+                />
+            </View>
+            {/*<View style={styles.inputView}>*/}
+            {/*    <TextInput*/}
+            {/*        style={styles.TextInput}*/}
+            {/*        placeholder="Password."*/}
+            {/*        placeholderTextColor="#003f5c"*/}
+            {/*        secureTextEntry={true}*/}
+            {/*        onChangeText={(password) => setPassword(password)}*/}
+            {/*    />*/}
+            {/*</View>*/}
+            {/*<TouchableOpacity>*/}
+            {/*    <Text style={styles.forgot_button}>Forgot Password?</Text>*/}
+            {/*</TouchableOpacity>*/}
+            <TouchableOpacity style={styles.loginBtn}>
+                <Text>LOGIN</Text>
+            </TouchableOpacity>
         </View>
-      </TouchableHighlight>
-      <View style={styles.countContainer}>
-        <Text style={styles.countText}>{count || null}</Text>
-      </View>
-    </View>
-  );
+    );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 10,
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10,
-  },
-  countContainer: {
-    alignItems: "center",
-    padding: 10,
-  },
-  countText: {
-    color: "#FF00FF",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    image: {
+        marginBottom: 40,
+    },
+    inputView: {
+        backgroundColor: "#46d2ff",
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+        alignItems: "center",
+    },
+    TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+    },
+    forgot_button: {
+        height: 30,
+        marginBottom: 30,
+    },
+    loginBtn: {
+        width: "80%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        backgroundColor: "#00b8ff",
+    },
+    statusbar: {
+        backgroundColor: "#00b8ff",
+    }
 });
