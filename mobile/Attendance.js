cedd2a30c8be05eefa337229e2d731654badd213
ThibaultGrt <thibault.garrot@gmail.com>
2023-02-21 16:51:45 +0100

    Mock de l'écriture de la feuille sur un tag NFC

--- mobile/Attendance.js
+++ mobile/Attendance.js
@@ -11,6 +11,10 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import axios from "axios";
 import { BASE_URL } from "./global";
+import NfcManager, {NfcTech, Ndef} from 'react-native-nfc-manager';
+import Toast from 'react-native-simple-toast';
+
+
 
 const wsUrl = BASE_URL + "/sheetUpdate";
 
@@ -21,6 +25,7 @@ const Attendance = ({ navigation, route }) => {
   const [readyToSign, setReadyToSign] = useState(false);
 
   const [teacherData, setTeacherData] = useState(route.params.teacherData);
+  const [isNFCActive, setIsNFCActive] = useState(false);
 
   const stopAttendance = () => {
     console.log(`${BASE_URL}/sheet/attendanceStop/${sheet.id}`);
@@ -64,6 +69,38 @@ const Attendance = ({ navigation, route }) => {
         console.log(e);
       });
   };
+
+  //TODO: ask for NFC permission
+  async function writeSheetOnNfcTag() {
+    let result = false;
+    setIsNFCActive(true);
+    try {
+      // STEP 1
+      //Need to request Ndef technology before writing on NFC tag
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      const bytes = Ndef.encodeMessage([Ndef.textRecord(sheet.toString())]);
+      if (bytes) {
+        await NfcManager.ndefHandler // STEP 2
+            .writeNdefMessage(bytes); // STEP 3
+        result = true;
+        Toast.show("Feuille écrite sur le tag NFC", Toast.LONG);
+      }
+    } catch (e) {
+      // console.warn("NFC error: ", e);
+      Toast.show('Réponse mock, mais voici l\'erreur : '+e, Toast.LONG);
+    } finally {
+      // STEP 4
+      await NfcManager.cancelTechnologyRequest();
+    }
+
+    return result;
+  }
+
+  //TODO: trouver une meilleure solution pour annuler l'écriture, car pour l'instant, on a une erreur car writeSheetOnNfcTag() reste bloqué et donc génère une erreur
+  async function cancelNfcWriting() {
+    setIsNFCActive(false);
+    await NfcManager.cancelTechnologyRequest();
+  }
 
   useEffect(() => {
     socket.on("connect", () => {
@@ -162,6 +199,16 @@ const Attendance = ({ navigation, route }) => {
               }}
             />
           )}
+          {!isNFCActive && (<Button
+              onPress={writeSheetOnNfcTag}
+              title="Write sheet on NFC tag"
+              accessibilityLabel="Write sheet on NFC tag"
+          />)}
+          {isNFCActive && (<Button
+              onPress={cancelNfcWriting}
+              title="Cancel NFC writing"
+              accessibilityLabel="Cancel NFC writing"
+          />)}
           {!readyToSign && (
             <Button
               onPress={stopAttendance}
