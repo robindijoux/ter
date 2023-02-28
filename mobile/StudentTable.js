2538a5d65d2e419efdd285e2a2f27f3ff1d8b4be
Robin Dijoux <dr@ecomail.fr>
2023-02-28 22:20:39 +0100

    Attendance - Fix de la liste des étudiant

--- mobile/StudentTable.js
+++ mobile/StudentTable.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
-import { View, Text, Switch } from "react-native";
-import { DataTable } from "react-native-paper";
+import React, { Fragment, useEffect } from "react";
+import { FlatList, Switch, View, Text } from "react-native";
 
 const CELL_COLOR_CODE = {
   PRESENT_CODE: "#77ddaa",
@@ -10,181 +9,181 @@ const CELL_COLOR_CODE = {
 };
 
 const StudentTable = ({ setParentForcedList, givenPresencePreview }) => {
-  const [presencePreview, setPresencePreview] =
-    React.useState(givenPresencePreview);
+  /**
+   * presencePreview is a map of <studentId, cellValue>
+   */
 
-  const [tableCells, setTableCells] = React.useState([]);
+  const [presencePreview, setPresencePreview] = React.useState([]);
+
+  const [editable, setEditable] = React.useState(false);
+
+  /**
+   * tableCells is an array of {
+   *    studentId,
+   *    color,
+   *    switchValue
+   * }
+   */
+  const [tableData, setTableData] = React.useState([]);
 
   /**
    * forcedList is a map of <studentId, cellValue>
    */
   const [forcedList, setForcedList] = React.useState({});
 
-  const [switchValues, setSwitchValues] = React.useState({});
-
   const onToggleSwitch = (studentId) => {
-    let newState = JSON.parse(JSON.stringify(switchValues));
-    console.log("before ToggleSwitch", studentId, newState);
-    newState[studentId] = !newState[studentId];
-    console.log("newState", JSON.stringify(newState, null, 2));
-    setSwitchValues(newState);
-    console.log("after ToggleSwitch", studentId, newState);
+    setPresencePreview((prevState) => ({
+      ...prevState,
+      [studentId]: {
+        ...prevState[studentId],
+        switchValue: !prevState[studentId].switchValue,
+        color: !prevState[studentId].switchValue
+          ? CELL_COLOR_CODE.PRESENT_CODE
+          : CELL_COLOR_CODE.ABSENT_CODE,
+      },
+    }));
   };
 
-  useEffect(
-    (prev) => {
-      // console.log("child prev switchValues", prev);
-      // console.log("child new switchValues", switchValues);
-    },
-    [switchValues]
-  );
+  useEffect(() => {
+    // console.log("re render");
+  });
 
   useEffect(() => {
-    setParentForcedList(forcedList);
-    console.log("child forcedList", forcedList);
-  }, [forcedList]);
-
-  useEffect(() => {
-    console.log("child presencePreview", presencePreview);
-
-    // construct the switch values
-    let newSwitchValues = Object.fromEntries(
-      Object.entries({
-        absent: presencePreview.absent,
-        present: presencePreview.present,
-        conflict: presencePreview.conflict,
-        pending: presencePreview.pending,
+    let newTableData = Object.entries(presencePreview).map(
+      ([studentId, colorAndValue]) => ({
+        studentId,
+        color: colorAndValue.color,
+        switchValue: colorAndValue.switchValue,
       })
-        .map(([key, idsList]) => {
-          // console.log("key", key, "idsList", idsList);
-          if (!presencePreview.editable) {
-            return idsList.map((studentId) => [studentId, undefined]);
-          }
-          switch (key) {
-            case "pending":
-              return idsList.map((studentId) => [studentId, false]);
-            case "absent":
-              return idsList.map((studentId) => [studentId, false]);
-            case "present":
-              return idsList.map((studentId) => [studentId, true]);
-            case "conflict":
-              return idsList.map((studentId) => [studentId, false]);
-            default:
-              return idsList.map((studentId) => [studentId, false]);
-          }
-        })
-        .flat()
     );
-
-    setSwitchValues(newSwitchValues);
-
-    let newTableCells = [];
-    let colorByStudentId = Object.entries(presencePreview)
-      .filter(([key, value]) => key !== "editable")
-      .map(([key, idList]) =>
-        idList.map((studentId) => {
-          let color;
-          switch (key) {
-            case "present":
-              color = CELL_COLOR_CODE.PRESENT_CODE;
-              break;
-            case "absent":
-              color = CELL_COLOR_CODE.ABSENT_CODE;
-              break;
-            case "conflict":
-              color = CELL_COLOR_CODE.WARNING_CODE;
-              break;
-            case "pending":
-              color = CELL_COLOR_CODE.PENDING_CODE;
-              break;
-            default:
-              color = CELL_COLOR_CODE.WARNING_CODE;
-              break;
-          }
-          return [studentId, color];
-        })
-      )
-      .flat();
-
-    console.log("colorByStudentId", colorByStudentId);
-    for (let [studentId, color] of colorByStudentId) {
-      newTableCells.push(
-        <DataTable.Row
-          key={studentId}
-          style={{
-            backgroundColor: color,
-          }}
-        >
-          <DataTable.Cell
-            style={{
-              justifyContent: "center",
-            }}
-          >
-            {studentId}
-          </DataTable.Cell>
-          <DataTable.Cell
-            style={{
-              justifyContent: "center",
-            }}
-          >
-            {presencePreview.editable ? (
-              // <Switch
-              //   disabled={!presencePreview.editable}
-              //   value={switchValues[studentId]}
-              //   onValueChange={() => {
-              //     let newVal = JSON.parse(JSON.stringify(switchValues));
-              //     console.log("prev val", newVal);
-              //     newVal[studentId] = !newVal[studentId];
-              //     console.log("new val", newVal);
-              //     setSwitchValues(newVal);
-              //     // setToggleVal(!toggleVal);
-              //   }}
-              // />
-              <Switch
-                trackColor={{ false: "#767577", true: "#81b0ff" }}
-                ios_backgroundColor="#3e3e3e"
-                onValueChange={() => {
-                  onToggleSwitch(studentId);
-                }}
-                value={switchValues[studentId]}
-              />
-            ) : (
-              "|"
-            )}
-          </DataTable.Cell>
-        </DataTable.Row>
-      );
-    }
-    setTableCells(newTableCells);
+    // console.log("newTableData", newTableData);
+    setTableData(newTableData);
+    setEditable(givenPresencePreview.editable);
   }, [presencePreview]);
 
   useEffect(() => {
-    setPresencePreview(givenPresencePreview);
+    console.log("editable", editable);
+  }, [editable]);
+
+  useEffect(() => {
+    setParentForcedList(forcedList);
+    // console.log("child forcedList", forcedList);
+  }, [forcedList]);
+
+  useEffect(() => {
+    /**
+     * newPresencePreview is a map of <studentId, {
+     *  color,
+     *  switchValue
+     * }>
+     */
+    let newPresencePreview = Object.fromEntries(
+      Object.entries(givenPresencePreview)
+        .filter(([key, value]) => key !== "editable")
+        .map(([key, idList]) =>
+          idList.map((studentId) => {
+            let color, switchValue;
+            switch (key) {
+              case "present":
+                color = CELL_COLOR_CODE.PRESENT_CODE;
+                switchValue = true;
+                break;
+              case "absent":
+                color = CELL_COLOR_CODE.ABSENT_CODE;
+                switchValue = false;
+                break;
+              case "conflict":
+                color = CELL_COLOR_CODE.WARNING_CODE;
+                switchValue = false;
+                break;
+              case "pending":
+                color = CELL_COLOR_CODE.PENDING_CODE;
+                switchValue = false;
+                break;
+              default:
+                color = CELL_COLOR_CODE.WARNING_CODE;
+                switchValue = false;
+                break;
+            }
+            return [studentId, { color, switchValue }];
+          })
+        )
+        .flat()
+    );
+    // console.log(
+    //   "newPresencePreview2",
+    //   JSON.stringify(newPresencePreview, null, 2)
+    // );
+    setPresencePreview(newPresencePreview);
   }, []);
 
   return (
     <View>
-      <DataTable>
-        <DataTable.Header>
-          <DataTable.Title
-            style={{
-              justifyContent: "center",
-            }}
-          >
-            Id
-          </DataTable.Title>
-          <DataTable.Title
-            style={{
-              justifyContent: "center",
-            }}
-          >
-            Presence
-          </DataTable.Title>
-        </DataTable.Header>
-        {tableCells}
-      </DataTable>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          marginTop: 10,
+          marginBottom: 10,
+        }}
+      >
+        <Text>Student</Text>
+        <Text>Presence</Text>
+      </View>
+      <FlatList
+        data={tableData}
+        renderItem={({ item }) => {
+          return (
+            <Item
+              studentId={item.studentId}
+              onToggleSwitch={() => onToggleSwitch(item.studentId)}
+              switchValue={item.switchValue}
+              color={item.color}
+              showSwitchButton={editable}
+            />
+          );
+        }}
+        keyExtractor={(item) => item.studentId + "" + item.color}
+      />
     </View>
   );
 };
+
+const Item = ({
+  studentId,
+  switchValue,
+  color,
+  onToggleSwitch,
+  showSwitchButton,
+}) => (
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-around",
+      borderTopColor: "black",
+      borderTopWidth: 0.5,
+      padding: 10,
+      backgroundColor: color,
+    }}
+  >
+    <Text
+      style={{
+        flex: 0.5,
+      }}
+    >
+      {studentId}
+    </Text>
+
+    <Switch
+      disabled={!showSwitchButton}
+      trackColor={{ false: "#767577", true: "#81b0ff" }}
+      ios_backgroundColor="#3e3e3e"
+      onValueChange={() => {
+        onToggleSwitch(studentId);
+      }}
+      value={switchValue}
+    />
+  </View>
+);
 
 export default StudentTable;
